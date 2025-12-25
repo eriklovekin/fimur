e1f5aaf35c626f45245016c8de7b1861a8419313
eriklovekin <erikslovekin@gmail.com>
2025-12-25 15:17:53 -0800

    rgb blinky running

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -10,7 +10,10 @@
 use defmt::info;
 use esp_hal::clock::CpuClock;
 use esp_hal::main;
-use esp_hal::time::{Duration, Instant};
+use esp_hal::rmt::Rmt;
+use esp_hal::time::{Duration, Instant, Rate};
+use esp_hal_smartled::{SmartLedsAdapter, smart_led_buffer};
+use smart_leds::{RGB8, SmartLedsWrite as _};
 use {esp_backtrace as _, esp_println as _};
 
 // This creates a default app-descriptor required by the esp-idf bootloader.
@@ -27,11 +30,22 @@ fn main() -> ! {
 
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
     let _peripherals = esp_hal::init(config);
+    let rmt = Rmt::new(_peripherals.RMT, Rate::from_mhz(80)).unwrap();
+    let mut led_buf = smart_led_buffer!(1);
+    let mut led = SmartLedsAdapter::new(rmt.channel0, _peripherals.GPIO8, &mut led_buf);
 
+    const LEVEL: u8 = 10;
+    let mut color = RGB8::default();
+    color.r = LEVEL;
     loop {
         info!("Hello world!");
+        led.write([color].into_iter()).unwrap();
         let delay_start = Instant::now();
         while delay_start.elapsed() < Duration::from_millis(500) {}
+        let tmp = color.r;
+        color.r = color.b;
+        color.b = color.g;
+        color.g = tmp;
     }
 
     // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v~1.0/examples
