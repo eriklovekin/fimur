e7e900ace1b56f4e938b440421fcb6faaeed4fb8
eriklovekin <erikslovekin@gmail.com>
2026-08-15 14:06:01 -0700

    before switching all fusion-core fields to SMatrix

--- fusion-core/src/lib.rs
+++ fusion-core/src/lib.rs
@@ -2,6 +2,7 @@
 #![no_main]
 
 use nalgebra::{
+    Vector3,
     Matrix3, 
     SMatrix, 
     stack,
@@ -12,10 +13,17 @@ const THREE_N: usize = 3*N_IMUS;
 const THREE_N_M3: usize = THREE_N-3;
 const THREE_P_M3: usize = THREE_N+3;
 
+
+/// Implementation of [Ref 8](https://hollydinkel.github.io/assets/pdf/AAS2025.pdf)
 pub struct FusionCore {
-    /// N+: Moore-Penrose inverse of N, 
-    /// where N is a 3mx3 matrix rotating virtual frame 
-    /// to the frame of sensor i
+    /// N: 3mx3 matrix rotating Filter frame 
+    /// to Sensor frame i
+    n: [Matrix3<f64>;N_IMUS],
+
+    /// p: 3mx1 position vector of Sensor i in Filter frame
+    p: [Vector3<f64>;N_IMUS],
+
+    /// N+: Moore-Penrose inverse of N
     np: [Matrix3<f64>;N_IMUS],
 
     /// T: Eliminates Euler acceleration effects
@@ -25,16 +33,30 @@ pub struct FusionCore {
 impl FusionCore {
     pub fn new() -> Self {
         Self {
+            n: [Matrix3::new(
+                    0.0,0.0,0.0,
+                    0.0,0.0,0.0,
+                    0.0,0.0,0.0);
+                    N_IMUS],
+            p: [Vector3::new(
+                    0.0,0.0,0.0);
+                    N_IMUS],
             np: [Matrix3::new(
                     0.0,0.0,0.0,
                     0.0,0.0,0.0,
                     0.0,0.0,0.0);
                     N_IMUS],
             t: SMatrix::zeros(),
+            
         }
     }
 
-    /// Populate Np and T fields of struct
+    pub fn fuse(&self, a: &[Vector3<f64>;N_IMUS], w: &[Vector3<f64>;N_IMUS]) -> (Vector3<f64>, Vector3<f64>) {
+    // pub fn fuse(&self, a: &SMatrix<f64,THREE_N,1>, w: &SMatrix<f64,THREE_N,1>) -> (SMatrix<f64,THREE_N,1>, SMatrix<f64,THREE_N,1>) {
+        let mut av: Vector3<f64> = self.np * *a;
+    }
+
+    /// Populate N, p, Np and T fields of struct
     /// n: 3n x 3 matrix stack of DCMs mapping filter frame to Sensor frame of ith sensor (f2s)
     /// p: 3n x 1 matrix stack of position of ith sensor in Filter frame
     pub fn init_geom(&mut self, n: &SMatrix<f64, THREE_N, 3>, p: &SMatrix<f64,THREE_N,1>) {
@@ -43,7 +65,6 @@ impl FusionCore {
         let zt = self.compute_zt(&y);
         debug_assert!((zt * y).norm() < 1e-9, "Z is not orthogonal to Y — null space extraction failed");
         self.t = self.compute_t(n,&zt);
-
     }
 
     fn compute_np(&self, n: &SMatrix<f64, THREE_N,3>) -> [Matrix3<f64>;N_IMUS] {
@@ -68,8 +89,7 @@ impl FusionCore {
         for i in 0..N_IMUS {
             let ri: Matrix3<f64> = n.fixed_view::<3, 3>(i*3, 0).into_owned();
             let pi = p.fixed_view::<3, 1>(i*3, 0).into_owned();
-            let ss_pi: Matrix3<f64> = self.skew_symmetric(&pi);
-            let product: Matrix3<f64> = ri*ss_pi;
+            let product: Matrix3<f64> = ri*pi.cross_matrix();
             ret.fixed_view_mut::<3, 3>(i*3, 0).copy_from(&product);        
         }
         ret
@@ -96,11 +116,17 @@ impl FusionCore {
         pseudo_inv*zt
     }
 
-    fn skew_symmetric(&self, v: &SMatrix<f64,3,1>) -> Matrix3<f64> {
-    Matrix3::new(
-         0.0, -v[2],  v[1],
-        v[2],  0.0, -v[0],
-       -v[1],  v[0],  0.0,
-        )
+    pub fn s(&self,x: &SMatrix<f64,THREE_N,1>) -> SMatrix<f64,THREE_N,1> {
+        let mut ret: SMatrix<f64,THREE_N,1> = SMatrix::zeros();
+        for i in 0..N_IMUS {
+            let xi = x.fixed_view::<3, 1>(i*3, 0).into_owned();
+            let product:SMatrix<f64,3,1> = 
+                self.n[i] *
+                xi.cross_matrix() *
+                xi.cross_matrix() *
+                self.p[i];
+            ret.fixed_view_mut::<3, 1>(i*3, 0).copy_from(&product);        
+        }
+        ret
     }
 }
